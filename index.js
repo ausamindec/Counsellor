1c5aa6cdf37fc8e9c1a18101cda91e0f1ad0e99f
ausamindec <ausamindec@gmail.com>
2022-05-17 11:50:54 +0530

    Adding index file

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const dbConnect = require("./config/db");
+const app = require("./app");
+const cloudinary = require("cloudinary");
+// DB Connect
+
+dbConnect();
+
+// Cloudinary
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`App is running on PORT ${process.env.PORT}`)
+);
